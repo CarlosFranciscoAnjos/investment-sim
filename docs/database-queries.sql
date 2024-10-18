0d1a3917c02189c5ff03e246e6e2e8122b3a82c6
Carlos Francisco Anjos <carlos.francisco.anjos@outlook.com>
2024-10-18 22:39:49 +0100

    Implement database procedure(s) to retrieve and print user information

--- docs/database-queries.sql
+++ docs/database-queries.sql
@@ -93,6 +93,7 @@ WHERE l.asset_id = 1;
 
 -- Print information
 ------------------------------
+
 CREATE OR REPLACE PROCEDURE pg_temp.print_space(v_size INT DEFAULT 1) AS $$
     BEGIN
 		FOR i IN 1..v_size LOOP
@@ -101,30 +102,35 @@ CREATE OR REPLACE PROCEDURE pg_temp.print_space(v_size INT DEFAULT 1) AS $$
 	END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE PROCEDURE pg_temp.print_separator(v_size INT DEFAULT 120) AS $$
+CREATE OR REPLACE FUNCTION pg_temp.print_separator(v_size INT DEFAULT 120, v_char VARCHAR(1) DEFAULT '-')
+RETURNS VOID AS $$
     BEGIN
-		RAISE NOTICE '%', repeat('-', v_size);
+		RAISE NOTICE '%', repeat(v_char, v_size);
 	END;
 $$ LANGUAGE plpgsql;
 
 -- Simulations
-CREATE OR REPLACE PROCEDURE pg_temp.print_simulations_header() AS $$
+CREATE OR REPLACE PROCEDURE pg_temp.print_simulations_header(v_id BIGINT DEFAULT -1) AS $$
     BEGIN
 		-- Define header and separator
-		CALL pg_temp.print_space(2);
-		RAISE NOTICE 'Simulations';
-		CALL pg_temp.print_separator();
+		CALL pg_temp.print_space();
+		IF v_id > 0 THEN
+			RAISE NOTICE '%', format('Simulation (id=%s)', v_id);
+		ELSE
+			RAISE NOTICE 'Simulations';
+		END IF;
+		EXECUTE pg_temp.print_separator();
 		RAISE NOTICE '%', format('%-15s | %-15s | %-50s', 'simulation_id', 'container_id', 'description');
-		CALL pg_temp.print_separator();
+		EXECUTE pg_temp.print_separator();
 	END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE PROCEDURE pg_temp.format_print_simulations(v_simulation_id BIGINT, v_container_id BIGINT, v_description VARCHAR) AS $$
+CREATE OR REPLACE PROCEDURE pg_temp.format_print_simulations(p_simulation RECORD) AS $$
     BEGIN
 	    RAISE NOTICE '%', format('%-15s | %-15s | %-50s',
-                        v_simulation_id, 
-                        v_container_id, 
-                        v_description);
+                        p_simulation.id, 
+                        p_simulation.container_id, 
+                        p_simulation.description);
 	END;
 $$ LANGUAGE plpgsql;
 
@@ -132,42 +138,69 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE PROCEDURE pg_temp.print_assets_header() AS $$
     BEGIN
 		-- Define header and separator
-		CALL pg_temp.print_space(2);
+		CALL pg_temp.print_space();
 		RAISE NOTICE 'Assets';
-		CALL pg_temp.print_separator();
+		EXECUTE pg_temp.print_separator();
 		RAISE NOTICE '%', format('%-15s | %-15s | %-15s | %-15s | %-50s', 'asset_id', 'container_id', 'value', 'income', 'description');
-		CALL pg_temp.print_separator();
+		EXECUTE pg_temp.print_separator();
 	END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE PROCEDURE pg_temp.format_print_assets(v_user_id BIGINT, v_container_id BIGINT, v_value FLOAT, v_income FLOAT, v_description VARCHAR) AS $$
+CREATE OR REPLACE PROCEDURE pg_temp.format_print_assets(p_asset RECORD) AS $$
     BEGIN
 	    RAISE NOTICE '%', format('%-15s | %-15s | %-15s | %-15s | %-50s',
-                        v_user_id, 
-                        v_container_id, 
-                        v_value, 
-                        v_income, 
-                        v_description);
+                        p_asset.id, 
+                        p_asset.container_id, 
+                        p_asset.value, 
+                        p_asset.income, 
+                        p_asset.description);
 	END;
 $$ LANGUAGE plpgsql;
 
--- Main function
-CREATE OR REPLACE PROCEDURE pg_temp.retrieve_print_all_user_information(p_username VARCHAR)
-AS $$
-DECLARE
-	v_user_id BIGINT;	
-    r_simulation RECORD;
+-- Secondary function(s)
+CREATE OR REPLACE PROCEDURE pg_temp.retrieve_print_all_simulation_information(p_simulation RECORD) AS $$
+DECLARE    
     r_asset RECORD;
 BEGIN
+		CALL pg_temp.print_space();
+		EXECUTE pg_temp.print_separator(v_char := '=');	
+
+		-- Select and print simulation information
+		CALL pg_temp.print_simulations_header(p_simulation.id);
+		CALL pg_temp.format_print_simulations(p_simulation);
+
+		-- Select and print assets
+		CALL pg_temp.print_assets_header();
+	    FOR r_asset IN
+	        SELECT a.id, a.container_id, a.value, a.income, a.description 
+	        FROM assets a
+	        WHERE a.container_id IN (
+	            SELECT s.container_id
+	            FROM simulations s
+	            WHERE s.id = p_simulation.id
+	        )
+			ORDER BY a.id
+	    LOOP
+	        CALL pg_temp.format_print_assets(r_asset);
+	    END LOOP;
+		CALL pg_temp.print_space();
+		EXECUTE pg_temp.print_separator(v_char := '=');	
+	END;
+$$ LANGUAGE plpgsql;
+
+
+-- Main function
+CREATE OR REPLACE PROCEDURE pg_temp.retrieve_print_all_user_information(p_username VARCHAR, p_simulation_id BIGINT DEFAULT -1)
+AS $$
+DECLARE
+	v_user_id BIGINT;
+    r_simulation RECORD;
+BEGIN
 	-- Print information about procedure and variables
-	CALL pg_temp.print_space(2);
-	CALL pg_temp.print_separator();	
+	EXECUTE pg_temp.print_separator(v_char := '=');	
 	RAISE NOTICE 'Starting procedure...';
 	RAISE NOTICE 'Retrieving all user information (username = "%")', p_username;
-	CALL pg_temp.print_separator();
-
-    -- Select and print simulations
-	CALL pg_temp.print_simulations_header();
+	EXECUTE pg_temp.print_separator(v_char := '=');
 
 	-- Retrieve user_id
 	SELECT u.id
@@ -175,49 +208,48 @@ BEGIN
 	FROM users u
 	WHERE u.username = p_username;
 
+    -- Select and print simulations
+	CALL pg_temp.print_simulations_header();
+
     FOR r_simulation IN
-        SELECT s.id AS simulation_id, s.container_id, s.description
-        FROM simulations s
-        WHERE s.user_id = v_user_id
+		SELECT s.id, s.container_id, s.description	
+		FROM simulations s
+		WHERE s.user_id = v_user_id
 		ORDER BY s.id
     LOOP
-        CALL pg_temp.format_print_simulations(
-			r_simulation.simulation_id,
-            r_simulation.container_id, 
-			r_simulation.description
-		);
+        CALL pg_temp.format_print_simulations(r_simulation);
     END LOOP;
+	CALL pg_temp.print_space(2);
 
-    -- Select and print assets
-	CALL pg_temp.print_assets_header();
+	-- Select and print simulation(s) detailed information
+	SELECT s.id, s.container_id, s.description
+	INTO r_simulation
+	FROM simulations s
+	WHERE s.id = p_simulation_id;
 
-    FOR r_asset IN
-        SELECT a.id AS asset_id, a.container_id, a.value, a.income, a.description 
-        FROM assets a
-        WHERE a.container_id IN (
-            SELECT s.container_id
-            FROM simulations s
-            WHERE s.user_id = v_user_id
-        )
-		ORDER BY a.container_id, a.id
-    LOOP
-        CALL pg_temp.format_print_assets(
-			r_asset.asset_id, 
-			r_asset.container_id, 
-			r_asset.value,
-            r_asset.income, 
-			r_asset.description
-		);
-    END LOOP;
+	IF r_simulation IS NOT NULL THEN
+		CALL pg_temp.retrieve_print_all_simulation_information(r_simulation);
+		CALL pg_temp.print_space(2);
+	ELSE
+		FOR r_simulation IN
+			SELECT s.id, s.container_id, s.description	
+			FROM simulations s
+			WHERE s.user_id = v_user_id
+			ORDER BY s.id
+	    LOOP
+	        CALL pg_temp.retrieve_print_all_simulation_information(r_simulation);
+			CALL pg_temp.print_space(2);
+	    END LOOP;
+	END IF;
 
 	-- End procedure
-	CALL pg_temp.print_space(2);
-	RAISE NOTICE 'Finished procedure successfully...';
-	CALL pg_temp.print_separator();
+	CALL pg_temp.print_space();
+	RAISE NOTICE 'Finished procedure successfully!';
+	EXECUTE pg_temp.print_separator();
 
 END;
 $$ LANGUAGE plpgsql;
 
 -- Execute
-CALL pg_temp.retrieve_print_all_user_information('flow1');
+CALL pg_temp.retrieve_print_all_user_information('flow1', -1);
 ------------------------------
